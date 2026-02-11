afb0322a45437e1af6e089df3f149c9ca7cfeaf0
tearga00 <156562207+tearga00@users.noreply.github.com>
2026-02-11 17:05:13 +0800

    hehe
    pogi

--- send_mail.php
+++ send_mail.php
@@ -20,7 +20,7 @@ try {
 
     //Recipients
     $mail->setFrom('from@example.com', 'Mailer');         //Set the sender of the message (your email address)
-    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (your bae's email address)
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient (erlssn001@gmail.com)
 
     //Content
     $mail->isHTML(true);
