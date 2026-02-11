61a5a67b6cb86ea175600d1d2e4b72c4c16d2bd0
tearga00 <156562207+tearga00@users.noreply.github.com>
2026-02-11 17:24:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -91,13 +91,13 @@ function drawText() {
 
     if(frameNumber < 250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("Happiest Valentines sa'yo na pinaniwala ulit ako, hindi lang sa konsepto ng araw na to, pero yung kakayahan ko para makapagmahal at bigyang chansa na mahalin ka na rin", canvas.width/2, canvas.height/2);
         opacity = opacity + 0.01;
     }
     //fades out the text by decreasing the opacity
     if(frameNumber >= 250 && frameNumber < 500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        context.fillText("Happiest Valentines sa'yo na pinaniwala ulit ako, hindi lang sa konsepto ng araw na to, pero yung kakayahan ko para makapagmahal at bigyang chansa na mahalin ka na rin", canvas.width/2, canvas.height/2);
         opacity = opacity - 0.01;
     }
 
